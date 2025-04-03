c2c3d1a6fb9b52966e6ffd2ccc65519d14f0b498
remybms <112947392+remybms@users.noreply.github.com>
2025-04-03 19:18:06 +0200

    change main to configure for the photos

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/clients/clients_page.dart';
 import 'package:mobile/login/login_page.dart';
+import 'package:mobile/photos/photos.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final List<CameraDescription> cameras = await availableCameras();
+
+  runApp(MyApp(cameras: cameras));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final List<CameraDescription> cameras;
 
-  // This widget is the root of your application.
+  const MyApp({super.key, required this.cameras});
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      initialRoute: '/photos',
       routes: {
         '/': (context) => Clients(),
         '/login': (context) => Login(),
+        '/photos': (context) => TakePicture(cameras: cameras,)
       },
     );
   }
