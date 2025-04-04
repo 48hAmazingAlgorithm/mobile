b104d088749238350108edfe188265bbd10fd207
homerofsteel <lucas.vernageau@ynov.com>
2025-04-04 14:36:38 +0200

    :lipstick: fix style to clients/individus pages

--- lib/clients/clients_page.dart
+++ lib/clients/clients_page.dart
@@ -62,45 +62,7 @@ class _ClientsState extends State<Clients> {
         }
       ]
     },
-    {
-      "numero_client": 3,
-      "nom_client": "Roche & Co",
-      "individus": [
-        {
-          "id_individu": 301,
-          "Nom": "Tanguy",
-          "Prénom": "Marie",
-          "date_de_naissance": "1982-09-04",
-          "date_fin_validité_CNI": "2032-09-04",
-          "Numéro_CNI": "123987456"
-        },
-        {
-          "id_individu": 302,
-          "Nom": "Boucher",
-          "Prénom": "Emilie",
-          "date_de_naissance": "1990-06-20",
-          "date_fin_validité_CNI": "2030-06-20",
-          "Numéro_CNI": "963852741"
-        }
-      ]
-    },
-    {
-      "numero_client": 4,
-      "nom_client": "Morel & Partners",
-      "individus": [
-        {
-          "id_individu": 401,
-          "Nom": "Blanc",
-          "Prénom": "Julien",
-          "date_de_naissance": "1975-01-09",
-          "date_fin_validité_CNI": "2025-01-09",
-          "Numéro_CNI": "357159486"
-        }
-      ]
-    },
   ];
-
-  Map<int, bool> isHovered = {};
 
   @override
   Widget build(BuildContext context) {
@@ -125,49 +87,25 @@ class _ClientsState extends State<Clients> {
           itemBuilder: (context, index) {
             final client = clients[index];
 
-            return MouseRegion(
-              onEnter: (_) => setState(() => isHovered[index] = true),
-              onExit: (_) => setState(() => isHovered[index] = false),
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 200),
-                transform: isHovered[index] == true
-                    ? Matrix4.translationValues(0, -3, 0)
-                    : Matrix4.identity(),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  boxShadow: isHovered[index] == true
-                      ? [
-                          BoxShadow(
-                            color: Colors.teal.withOpacity(0.7),
-                            spreadRadius: 2,
-                          )
-                        ]
-                      : [
-                          BoxShadow(
-                            color: Colors.teal.withOpacity(0.4),
-                            spreadRadius: 1,
-                          )
-                        ],
+            return Card(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: ListTile(
+                contentPadding: EdgeInsets.all(16),
+                leading: Icon(Icons.business, color: Colors.teal, size: 40),
+                title: Text(
+                  client['nom_client'] as String,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-                margin: EdgeInsets.symmetric(vertical: 10),
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(16),
-                  leading: Icon(Icons.business, color: Colors.teal, size: 40),
-                  title: Text(
-                    client['nom_client'] as String,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  onTap: () async {
-                    await showDialog(
-                      context: context,
-                      builder: (context) => Individu(
-                        nomClient: client['nom_client'] as String,
-                        individus: client['individus']
-                            as List<Map<String, Object>>,
-                      ),
-                    );
-                  },
-                ),
+                onTap: () async {
+                  await showDialog(
+                    context: context,
+                    builder: (context) => Individu(
+                      nomClient: client['nom_client'] as String,
+                      individus: client['individus']
+                          as List<Map<String, Object>>,
+                    ),
+                  );
+                },
               ),
             );
           },
--- lib/clients/clients_popup.dart
+++ lib/clients/clients_popup.dart
@@ -1,151 +1,95 @@
 import 'package:flutter/material.dart';
 
-class Individu extends StatefulWidget {
+class Individu extends StatelessWidget {
   final List<Map<String, Object>> individus;
   final String nomClient;
 
   Individu({super.key, required this.individus, required this.nomClient});
 
   @override
-  _IndividuState createState() => _IndividuState();
-}
-
-class _IndividuState extends State<Individu> {
-  Map<int, bool> isHovered = {}; 
-  bool isButtonHovered = false; 
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 30, 
-            left: 20,
-            right: 20,
-            child: Container(
-              padding: EdgeInsets.all(10), 
+      appBar: AppBar(
+        automaticallyImplyLeading: false, 
+        title: Text(
+          "Membres du Groupe",
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+        toolbarHeight: 100,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(15),
                 boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 10)],
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Row(
                 children: [
-                  Row(
-                    children: [
-                      Icon(Icons.business, color: Colors.teal, size: 40),
-                      SizedBox(width: 10),
-                      Text(
-                        widget.nomClient,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  SizedBox(
-                    height: 500, 
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: widget.individus.length,
-                      itemBuilder: (context, index) {
-                        final individu = widget.individus[index];
-
-                        return MouseRegion(
-                          onEnter: (_) => setState(() => isHovered[index] = true),
-                          onExit: (_) => setState(() => isHovered[index] = false),
-                          child: AnimatedContainer(
-                            duration: Duration(milliseconds: 150),
-                            transform: isHovered[index] == true
-                                ? Matrix4.translationValues(0, -5, 0)
-                                : Matrix4.identity(),
-                            margin: EdgeInsets.symmetric(vertical: 4),
-                            decoration: BoxDecoration(
-                              color: isHovered[index] == true
-                                  ? Colors.blueGrey.withOpacity(0.1)
-                                  : Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow: [
-                                if (isHovered[index] == true)
-                                  BoxShadow(
-                                    color: Colors.blueGrey.withOpacity(0.3),
-                                    spreadRadius: 2,
-                                  )
-                              ],
-                            ),
-                            child: InkWell(
-                              borderRadius: BorderRadius.circular(10),
-                              splashColor: Colors.blueGrey.withOpacity(0.3),
-                              hoverColor: Colors.blueGrey.withOpacity(0.1),
-                              onTap: () {
-                                Navigator.pushNamed(context, '/photos');
-                              },
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                                child: Row(
-                                  children: [
-                                    Icon(Icons.person, color: Colors.blueGrey, size: 45),
-                                    SizedBox(width: 10),
-                                    Column(
-                                      children: [
-                                        Text(
-                                          "${individu['Nom']} ${individu['Prénom']}",
-                                          style: TextStyle(
-                                              fontSize: 22, fontWeight: FontWeight.w600),
-                                        ),
-                                        Text(
-                                          "Né(e) le : ${individu['date_de_naissance']}",
-                                          style: TextStyle(
-                                              fontSize: 15, color: Colors.grey[700]),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
+                  Icon(Icons.business, color: Colors.teal, size: 50),
+                  SizedBox(width: 15),
+                  Text(
+                    nomClient,
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
                   ),
                 ],
               ),
             ),
-          ),
-          Positioned(
-            bottom: 25,
-            left: MediaQuery.of(context).size.width / 2 - 250 / 2, 
-            child: MouseRegion(
-              onEnter: (_) => setState(() => isButtonHovered = true),
-              onExit: (_) => setState(() => isButtonHovered = false),
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 200),
-                transform: isButtonHovered
-                    ? Matrix4.translationValues(0, -5, 0)
-                    : Matrix4.identity(),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(); 
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(250, 60),
-                    backgroundColor: Colors.teal,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: individus.length,
+                itemBuilder: (context, index) {
+                  final individu = individus[index];
+                  return Card(
+                    margin: EdgeInsets.symmetric(vertical: 8),
+                    child: ListTile(
+                      contentPadding: EdgeInsets.all(16),
+                      leading: Icon(Icons.person, color: Colors.blueGrey, size: 45),
+                      title: Text(
+                        "${individu['Nom']} ${individu['Prénom']}",
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        "Né(e) le : ${individu['date_de_naissance']}",
+                        style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+                      ),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/photos');
+                      },
                     ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(250, 60),
+                  backgroundColor: Colors.teal,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  child: Text(
-                    'Retour',
-                    style: TextStyle(fontSize: 30, color: Colors.white),
-                  ),
+                ),
+                child: Text(
+                  'Retour',
+                  style: TextStyle(fontSize: 24, color: Colors.white),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
